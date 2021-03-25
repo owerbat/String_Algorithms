e69d9f27d55c40fa1ef119f0e991488e369eddd5
owerbat <niktimak@gmail.com>
2021-03-25 10:31:48 +0300

    Prefix function

--- 1_prefix_function/main.cpp
+++ 1_prefix_function/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+void PrefixFunction(std::string& in, std::vector<size_t>& prefix_values) {
+    prefix_values[0] = 0;
+
+    for (size_t i = 1; i < in.size(); ++i) {
+        size_t next = prefix_values[i - 1];
+        while ((in[next] != in[i]) && next > 0) {
+            next = prefix_values[next - 1];
+        }
+        if (in[next] == in[i]) {
+            prefix_values[i] = next + 1;
+        } else {
+            prefix_values[i] = 0;
+        }
+    }
+}
 
 int main() {
-    std::cout << "hello\n";
+    std::string in;
+    std::cin >> in;
+
+    std::vector<size_t> prefix_values{in.size(), std::allocator<size_t>{}};
+    PrefixFunction(in, prefix_values);
+
+    for (auto& value : prefix_values) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
 }
